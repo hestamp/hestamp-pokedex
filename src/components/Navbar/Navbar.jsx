2839da7dab0abf3911c75f01f8a4b5fe8ebd6ad4
Tom Hestamp <tom@hestamp.me>
2023-08-10 20:45:03 +0300

    implemented sorting

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import styles from './Navbar.module.css'
+import { useNavigate } from 'react-router-dom'
 const Navbar = ({ context }) => {
+  const navigate = useNavigate()
   return (
-    <div className={styles.navbar}>{context ? context : <h1>Pokedex</h1>}</div>
+    <div className={styles.navbar}>
+      {context ? context : <h1 onClick={() => navigate('/')}>PokeStamp</h1>}
+    </div>
   )
 }
 
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,22 +4,58 @@ import MyLoader from '../../components/Navbar/Tools/MyLoader/MyLoader'
 import myAxios from '../../axios'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
-import { uIsErr, uPokeTypes, uPokemonArray } from '../../store/tempSlice'
+import {
+  uCurrentPokemon,
+  uIsErr,
+  uPokeTypes,
+  uPokemonArray,
+} from '../../store/tempSlice'
 import MyDropdown from '../../components/Navbar/Tools/MyDropdown/MyDropdown'
 import MyCheckbox from '../../components/Navbar/Tools/MyCheckbox/MyCheckbox'
+import { useNavigate } from 'react-router-dom'
 const MainPage = () => {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const limit = 16
   const [loadedTimes, setLoadedTimes] = useState(0)
   const [pickedPokemon, setPickedPokemon] = useState(null)
   const [pickedId, setPickedId] = useState(null)
   const [currentPoke, setCurrentPoke] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
-  const [selectedType, setSelectedType] = useState(null)
-  const [isChecked, setIsChecked] = useState(false)
+  const [selectedType, setSelectedType] = useState('')
+  const [isChecked, setIsChecked] = useState(true)
+  const [filteredArray, setFilteredArray] = useState([])
 
   const handleCheckboxChange = (value) => {
     setIsChecked(value)
+  }
+
+  const getTypeColor = (typeName) => {
+    const typeColors = {
+      normal: '#A8A8A8',
+      fighting: '#D67873',
+      flying: '#83A2E3',
+      poison: '#C183C1',
+      ground: '#E0C791',
+      rock: '#C9BB8A',
+      bug: '#C2D21F',
+      ghost: '#8571BE',
+      steel: '#CCCCCC',
+      fire: '#FF9A58',
+      water: '#6A92E3',
+      grass: '#79C957',
+      electric: '#FFD54F',
+      psychic: '#FF80B2',
+      ice: '#AEE3FF',
+      dragon: '#8A75FF',
+      dark: '#8E8E99',
+      fairy: '#FFA1E3',
+      unknown: '#B0B0B0',
+      shadow: '#6B6B6B',
+    }
+
+    const typeNameLower = typeName.toLowerCase()
+    return typeColors[typeNameLower] || null
   }
 
   const repoGif =
@@ -34,16 +70,35 @@ const MainPage = () => {
       setPickedPokemon(null)
       setPickedId(null)
       setCurrentPoke(null)
+      dispatch(uCurrentPokemon(null))
     } else {
       setPickedPokemon(item)
       setPickedId(pokeId)
       setCurrentPoke(poke)
+      dispatch(uCurrentPokemon(poke))
+    }
+
+    if (window.innerWidth <= 1200) {
+      //
+      navigate(`/pokemon/${pokeId}`)
     }
   }
 
   const handleTypeSelect = (type) => {
     setSelectedType(type)
-    // onTypeSelect(type)
+    console.log(type)
+    onTypeSelect(type)
+  }
+
+  const onTypeSelect = (item) => {
+    const filteredArr = pokemonArray.filter((pokemon) => {
+      if (pokemon.types) {
+        return pokemon.types.some((type) => item == type)
+      }
+      return false
+    })
+    console.log(filteredArr)
+    setFilteredArray(filteredArr)
   }
 
   const getAllPokemons = async () => {
@@ -66,20 +121,29 @@ const MainPage = () => {
 
         await Promise.all(
           updatedPokemonArray.map(async (pokemon) => {
-            const pokemonResponse = await axios.get(pokemon.url)
+            try {
+              const pokemonResponse = await axios.get(pokemon.url)
 
-            const updatedPokemon = {
-              ...pokemon,
-              rest: pokemonResponse.data,
+              const names = pokemonResponse.data.types.map(
+                ({ type }) => type.name
+              )
+
+              const updatedPokemon = {
+                ...pokemon,
+                types: names,
+                rest: pokemonResponse.data,
+              }
+
+              const lastUpdateArr = updatedArray.map((prevPokemon) =>
+                prevPokemon.url === pokemon.url ? updatedPokemon : prevPokemon
+              )
+
+              updatedArray = lastUpdateArr
+
+              dispatch(uPokemonArray(lastUpdateArr))
+            } catch (error) {
+              console.error('some error, sorry', error)
             }
-
-            const lastUpdateArr = updatedArray.map((prevPokemon) =>
-              prevPokemon.url === pokemon.url ? updatedPokemon : prevPokemon
-            )
-
-            updatedArray = lastUpdateArr
-
-            dispatch(uPokemonArray(lastUpdateArr))
           })
         )
       }
@@ -90,18 +154,21 @@ const MainPage = () => {
     } finally {
       setIsLoading(false)
       setLoadedTimes((prev) => prev + 1)
+      if (selectedType != '') {
+        handleTypeSelect(selectedType)
+      }
     }
   }
 
   const clickTag = (e, tag) => {
     e.stopPropagation()
-    console.log(tag)
+    handleTypeSelect(tag)
   }
 
   const getTypes = async () => {
     try {
       const response = await myAxios.get(`type?limit=999`)
-      console.log(response.data.results)
+
       if (response) {
         dispatch(uPokeTypes(response.data.results))
       }
@@ -119,6 +186,10 @@ const MainPage = () => {
       getTypes()
     }
   }, [])
+
+  const resetTypes = () => {
+    setSelectedType('')
+  }
 
   function extractIdFromUrl(url) {
     const match = url.match(/\/(\d+)\/$/)
@@ -142,13 +213,14 @@ const MainPage = () => {
           <MyCheckbox
             label="Animated"
             value={isChecked}
-            abrFull="Gif animations can load GPU of your device"
-            abbrTxt="Why?"
             onChange={handleCheckboxChange}
           />
         </div>
         <div className={styles.allPokemons}>
-          {!isErr && pokemonArray.length ? (
+          {!isErr &&
+          pokemonArray.length &&
+          filteredArray.length == 0 &&
+          selectedType == '' ? (
             pokemonArray.map((poke, id) => {
               const pokeId = extractIdFromUrl(poke.url)
 
@@ -164,22 +236,35 @@ const MainPage = () => {
                     <h3>{poke.name}</h3>
                   </div>
                   <div className={styles.imgDiv}>
-                    <img
-                      className={styles.pokeImg}
-                      src={`${repoGif}${pokeId}.gif`}
-                    />
+                    {isChecked ? (
+                      <img
+                        className={styles.pokeGif}
+                        src={`${repoGif}${pokeId}.gif`}
+                      />
+                    ) : (
+                      <img
+                        className={styles.pokeImg}
+                        src={poke.rest.sprites.other.home.front_default}
+                      />
+                    )}
                   </div>
                   {poke.rest ? (
                     <div className={styles.tagsBlock}>
-                      {poke.rest.types.map((type) => (
-                        <p
-                          onClick={(e) => clickTag(e, type.type.name)}
-                          className={styles.oneTag}
-                          key={type.type.name}
-                        >
-                          {type.type.name}
-                        </p>
-                      ))}
+                      {poke.rest.types.map((type) => {
+                        const colorPick = getTypeColor(type.type.name)
+                        return (
+                          <p
+                            style={{
+                              backgroundColor: colorPick,
+                            }}
+                            onClick={(e) => clickTag(e, type.type.name)}
+                            className={styles.oneTag}
+                            key={type.type.name}
+                          >
+                            {type.type.name}
+                          </p>
+                        )
+                      })}
                     </div>
                   ) : (
                     <div className={styles.loaderDiv}>
@@ -189,6 +274,67 @@ const MainPage = () => {
                 </div>
               )
             })
+          ) : !isErr && filteredArray.length ? (
+            filteredArray.map((poke, id) => {
+              const pokeId = extractIdFromUrl(poke.url)
+
+              return (
+                <div
+                  onClick={() => clickItem(id, pokeId, poke)}
+                  className={`${styles.pokeBlock} ${
+                    pickedPokemon == id && styles.pickedBlock
+                  }`}
+                  key={id}
+                >
+                  <div className={styles.nameAndTag}>
+                    <h3>{poke.name}</h3>
+                  </div>
+                  <div className={styles.imgDiv}>
+                    {isChecked ? (
+                      <img
+                        className={styles.pokeGif}
+                        src={`${repoGif}${pokeId}.gif`}
+                      />
+                    ) : (
+                      <img
+                        className={styles.pokeImg}
+                        src={poke.rest.sprites.other.home.front_default}
+                      />
+                    )}
+                  </div>
+                  {poke.rest ? (
+                    <div className={styles.tagsBlock}>
+                      {poke.rest.types.map((type) => {
+                        const colorPick = getTypeColor(type.type.name)
+                        return (
+                          <p
+                            style={{
+                              backgroundColor: colorPick,
+                            }}
+                            onClick={(e) => clickTag(e, type.type.name)}
+                            className={styles.oneTag}
+                            key={type.type.name}
+                          >
+                            {type.type.name}
+                          </p>
+                        )
+                      })}
+                    </div>
+                  ) : (
+                    <div className={styles.loaderDiv}>
+                      <MyLoader />
+                    </div>
+                  )}
+                </div>
+              )
+            })
+          ) : selectedType != '' && filteredArray.length == 0 ? (
+            <>
+              <h3 className={styles.noFlex}>No loaded pokemons this type</h3>
+              <button onClick={resetTypes} className={styles.myButtReset}>
+                Reset
+              </button>
+            </>
           ) : isErr ? (
             <p>Some error, please reload page</p>
           ) : (
